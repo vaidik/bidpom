066ee372a366acde05f72bd51d9879d7e33c666f
Dave Hunt <dave.hunt@gmail.com>
2012-07-18 15:40:30 +0100

    Added WebDriver test for reset password. Added mock user object for testing. Improved restmail support for multiple messages.
    Conflicts:
    	tests/webdriver/check_sign_in.py

--- pages/webdriver/sign_in.py
+++ pages/webdriver/sign_in.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 class SignIn(Base):
 
     _signed_in_email_locator = (By.CSS_SELECTOR, 'label[for=email_0]')
+    _this_is_not_me_locator = (By.ID, 'thisIsNotMe')
     _email_locator = (By.ID, 'email')
     _password_locator = (By.ID, 'password')
     _next_locator = (By.CSS_SELECTOR, 'button.start')
@@ -53,6 +54,12 @@ class SignIn(Base):
         """Get the value of the email that is currently signed in."""
         return self.selenium.find_element(*self._signed_in_email_locator).text
 
+    def click_this_is_not_me(self):
+        """Clicks the 'This is not me' button."""
+        self.selenium.find_element(*self._this_is_not_me_locator).click()
+        WebDriverWait(self.selenium, self.timeout).until(
+            lambda s: s.find_element(*self._email_locator).is_displayed())
+
     @property
     def email(self):
         """Get the value of the email field."""
@@ -82,7 +89,7 @@ class SignIn(Base):
         """Get the value of the verify password field."""
         return self.selenium.find_element(*self._verify_password_locator).text
 
-    @password.setter
+    @verify_password.setter
     def verify_password(self, value):
         """Set the value of the verify password field."""
         password = self.selenium.find_element(*self._verify_password_locator)
@@ -138,7 +145,7 @@ class SignIn(Base):
         self.selenium.find_element(*self._reset_password_locator).click()
         WebDriverWait(self.selenium, self.timeout).until(
             lambda s: s.find_element(
-                *self._use_another_email_address_locator).is_displayed())
+                *self._check_email_at_locator).is_displayed())
 
     def sign_in(self, email, password):
         """Signs in using the specified email address and password."""
--- tests/webdriver/check_sign_in.py
+++ tests/webdriver/check_sign_in.py
@@ -10,6 +10,7 @@ import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 
 from ... import BrowserID
+from ...mocks.user import MockUser
 from .. import restmail
 from base import BaseTest
 
@@ -37,11 +38,12 @@ class TestSignIn(BaseTest):
 
     @pytest.mark.travis
     def test_sign_in_new_user_helper(self, mozwebqa):
-        restmail_username = 'bidpom_%s' % uuid.uuid1()
-        browser_id = BrowserID(mozwebqa.selenium, mozwebqa.timeout)
-        print 'signing in as %s@restmail.net' % restmail_username
-        browser_id.sign_in('%s@restmail.net' % restmail_username)
-        mail = restmail.get_mail(restmail_username)
+        user = MockUser()
+        from ...pages.webdriver.sign_in import SignIn
+        signin = SignIn(mozwebqa.selenium, mozwebqa.timeout, expect='new')
+        print 'signing in as %s' % user.primary_email
+        signin.sign_in_new_user(user.primary_email, 'password')
+        mail = restmail.get_mail(user.id)
         assert 'Click to confirm this email address' in mail[0]['text']
 
     @pytest.mark.travis
