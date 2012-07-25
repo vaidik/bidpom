6d7adc40f31ff6441699331301004a8c10ecd942
Dave Hunt <dave.hunt@gmail.com>
2012-07-25 16:13:21 +0100

    Pass timeout value to restmail
    Conflicts:
    	tests/webdriver/check_sign_in.py

--- tests/webdriver/check_sign_in.py
+++ tests/webdriver/check_sign_in.py
@@ -43,24 +43,25 @@ class TestSignIn(BaseTest):
         signin = SignIn(mozwebqa.selenium, mozwebqa.timeout, expect='new')
         print 'signing in as %s' % user.primary_email
         signin.sign_in_new_user(user.primary_email, 'password')
-        mail = restmail.get_mail(user.id)
+        mail = restmail.get_mail(user.primary_email, timeout=mozwebqa.timeout)
         assert 'Click to confirm this email address' in mail[0]['text']
 
     @pytest.mark.travis
     def test_sign_in_new_user(self, mozwebqa):
-        restmail_username = 'bidpom_%s' % uuid.uuid1()
-
+        user = MockUser()
         from ...pages.webdriver.sign_in import SignIn
         signin = SignIn(mozwebqa.selenium, mozwebqa.timeout, expect='new')
-        print 'signing in as %s@restmail.net' % restmail_username
-        signin.email = '%s@restmail.net' % restmail_username
+        print 'signing in as %s' % user.primary_email
+        signin.email = user.primary_email
         signin.click_next(expect='verify')
+        signin.password = user.password
+        signin.verify_password = user.password
         signin.click_verify_email()
-        assert signin.check_email_at_address == email
+        assert signin.check_email_at_address == user.primary_email
 
         signin.close_window()
         signin.switch_to_main_window()
-        mail = restmail.get_mail(restmail_username)
+        mail = restmail.get_mail(user.primary_email, timeout=mozwebqa.timeout)
         assert 'Click to confirm this email address' in mail[0]['text']
 
     def test_sign_in_returning_user_helper(self, mozwebqa):
